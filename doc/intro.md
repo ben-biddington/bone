dea91bcaaade56461fc88d8605ad1241fe4babdd
Ben Biddington <ben.biddington@gmail.com>
2015-02-13 11:28:10 +1300

    Who says famine has to be depressing?

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to bone
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/bone/core_test.clj
@@ -0,0 +1,7 @@
+(ns bone.core-test
+  (:require [clojure.test :refer :all]
+            [bone.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
