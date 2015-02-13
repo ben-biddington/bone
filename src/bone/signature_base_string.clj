104c88e60f28b26f0dffe7f9876dab8ee383b1fc
Ben Biddington <ben.biddington@gmail.com>
2015-02-14 08:45:27 +1300

    Formatting

--- src/bone/signature_base_string.clj
+++ src/bone/signature_base_string.clj
@@ -4,20 +4,15 @@
   (:require [ring.util.codec :refer :all]))
 
 (defn- %[what] (ring.util.codec/url-encode what))
-
-(defn- sort-by-key-and-value[parameters] (into (sorted-map) parameters))
-
-(defn- join-as-string[param] (str (%(key param)) (% "=") (% (val param))))
-(defn- name-value-string[parameters] (map join-as-string parameters))
-
 (def ^{:private true} ignored-parameter-names #{"realm" "oauth_signature"})
-
-(defn- blacklisted? [item] (contains? ignored-parameter-names (key item)))
-
-(defn- white-list[parameters] (filter (complement blacklisted?) parameters))
-
 (def ^{:private true} ampersand "&")
 (def ^{:private true} url-encoded-ampersand (% "&"))
+
+(defn- sort-by-key-and-value[parameters] (into (sorted-map) parameters))
+(defn- join-as-string[param] (str (%(key param)) (% "=") (% (val param))))
+(defn- name-value-string[parameters] (map join-as-string parameters))
+(defn- blacklisted? [item] (contains? ignored-parameter-names (key item)))
+(defn- white-list[parameters] (filter (complement blacklisted?) parameters))
 
 (defn signature-base-string[args]
   (let [sorted-params (sort-by-key-and-value (white-list (:parameters args)))]
