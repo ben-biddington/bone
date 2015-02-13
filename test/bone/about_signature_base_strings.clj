736e5c7c57cedd9b608da1ea7b8acccac953654f
Ben Biddington <ben.biddington@gmail.com>
2015-02-13 17:03:29 +1300

    Formatting

--- test/bone/about_signature_base_strings.clj
+++ test/bone/about_signature_base_strings.clj
@@ -57,12 +57,11 @@
     "oauth_nonce"        "kllo9940pd9333jh"
     "file"               "vacation.jpg"
     "size"               "original"})]
+
   (let [result (signature-base-string parameters)]
     (is (= (str "file%3Dvacation.jpg%26oauth_consumer_key%3Ddpf43f3p2l4k3l03%26oauth_nonce%3Dkllo9940pd9333jh%26" 
                 "oauth_signature_method%3DHMAC-SHA1%26oauth_timestamp%3D1191242096%26oauth_token%3Dnnch734d00sl2jdk%26" 
-                "oauth_version%3D1.0%26size%3Doriginal") result))
-    ))
-)
+                "oauth_version%3D1.0%26size%3Doriginal") result)))))
 
 (deftest normalizing-request-parameters
   (let [result (signature-base-string example-parameters)]
