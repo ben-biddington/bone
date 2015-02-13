c8a02bf117a082234c337d47ca49ae4a7ee0a346
Ben Biddington <ben.biddington@gmail.com>
2015-02-14 08:49:37 +1300

    Refactor: => threading

--- src/bone/signature_base_string.clj
+++ src/bone/signature_base_string.clj
@@ -10,9 +10,10 @@
 
 (defn- sort-by-key-and-value[parameters] (into (sorted-map) parameters))
 (defn- join-as-string[param] (str (%(key param)) (% "=") (% (val param))))
-(defn- name-value-string[parameters] (map join-as-string parameters))
+(defn- name-value-pairs[parameters] (map join-as-string parameters))
 (defn- blacklisted? [item] (contains? ignored-parameter-names (key item)))
 (defn- white-list[parameters] (filter (complement blacklisted?) parameters))
+(defn- combine[name-value-pairs] (clojure.string/join url-encoded-ampersand name-value-pairs))
 
 (defn signature-base-string[args]
   (let [sorted-params (sort-by-key-and-value (white-list (:parameters args)))]
@@ -20,4 +21,4 @@
       (list 
         (% (:verb args))
         (% (:url args))
-        (clojure.string/join url-encoded-ampersand (name-value-string sorted-params))))))
+        (-> sorted-params name-value-pairs combine)))))
