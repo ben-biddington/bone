a029ddb01dee8998a3ba3ccd6ce8e96fc2e5c120
Ben Biddington <ben.biddington@gmail.com>
2015-02-20 12:33:09 +1300

    Refactor: extracted functions

--- test/bone/signing/examples.clj
+++ test/bone/signing/examples.clj
@@ -8,14 +8,14 @@
 ;; http://oauth.net/core/1.0a/#signing_process
 ;; HMAC-SHA1: http://oauth.net/core/1.0a/#RFC2104
 
+(defn- %64 [what] (org.apache.commons.codec.binary.Base64/encodeBase64 what))
+(defn- utf-8-str [what] (String. what "UTF-8"))
 (defn- hmac [^String data ^String key]
   (let [hmac-sha1 "HmacSHA1"
         secret (SecretKeySpec. (.getBytes key) hmac-sha1)
         mac (doto (Mac/getInstance hmac-sha1) (.init secret))]
 
-    (String. (org.apache.commons.codec.binary.Base64/encodeBase64 
-              (.doFinal mac (.getBytes data)))
-             "UTF-8")))
+    (utf-8-str (%64 (.doFinal mac (.getBytes data))))))
 
 (defn hmac-sha1-sign[base_string, secret] (hmac base_string secret))
 
