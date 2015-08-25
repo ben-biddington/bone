e5ef24d177f8942be03c0351eb982db72ce3e712
Ben Biddington <ben.biddington@gmail.com>
2015-08-25 12:06:10 +1200

    v0.4.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bone "0.3.0-SNAPSHOT"
+(defproject bone "0.4.0-SNAPSHOT"
   :description "Simple OAuth for clojure"
   :url "https://github.com/ben-biddington/bone"
   :license {:name "Eclipse Public License"
